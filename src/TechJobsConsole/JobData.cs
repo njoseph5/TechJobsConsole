355b89a7c08daae8f92f5f9f89b0094ab50c3868
Neethu Joseph <vabsaxena@gmail.com>
2017-11-12 14:20:11 -0600

    completed first todo's

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -38,6 +38,26 @@ namespace TechJobsConsole
             return values;
         }
 
+       public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            // load data, if not already loaded
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (KeyValuePair<string, string> column in row)
+                {
+                    if (column.Value.Contains(value)){
+                        jobs.Add(row);
+                        break;
+                    }
+                }
+            }
+            return jobs;
+        }
+
+
         public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
         {
             // load data, if not already loaded
